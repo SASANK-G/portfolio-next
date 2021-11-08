182f47ebcc1ca7248f79318048ad45a864c36746
SASANK-G <gsbnsrsasank@gmail.com>
2021-11-08 20:06:19 +0530

    framer motion-1

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,6 +1,8 @@
+import { motion } from "framer-motion";
 import { useState } from "react";
 import ProjectCard from "../components/ProjectCard";
 import { projects as projectsData } from "../data";
+import { fadeInUp, stagger } from "../animations";
 
 
 
@@ -8,16 +10,23 @@ const Projects = () => {
     
     return (
       <div className="px-5 py-2 overflow-y-scroll" style={{ height: "70vh" }}>
-        <div className="relative grid grid-cols-12 gap-4 my-3">
+        <motion.div className="relative grid grid-cols-12 gap-4 my-3"
+        variants={ stagger}
+        initial="initial"
+        animate = "animate"
+        >
           {
                 
           projectsData.map((project) => (
               
-            <div className="col-span-12 p-2 bg-gray-200 rounded-lg sm:col-span-6 lg:col-span-4 dark:bg-gray-800" key={project.name}>
-              <ProjectCard project={project} key={project.name} />
-            </div>
+            <motion.div className="col-span-12 p-2 bg-gray-200 rounded-lg sm:col-span-6 lg:col-span-4 dark:bg-gray-800" 
+            key={project.name}
+            variants={fadeInUp}
+            >
+              <ProjectCard project={project} />
+            </motion.div>
           ))}
-        </div>
+        </motion.div>
       </div>
     )
 }
